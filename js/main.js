a924705c0fb078af51283e7221fcf5a483d549cd
Vlad Starshinov <54811872+vladistarshinov@users.noreply.github.com>
2020-03-06 16:09:24 +0300

    Добавил рандомную генерацию кораблей, устранение коллизий

--- js/main.js
+++ js/main.js
@@ -9,25 +9,101 @@ const record = document.getElementById('record'),
 
 const game = {
     ships: [
-        {
-            location: ['26', '36', '46', '56'],
-            hit: ['', '', '', '']
-        },
-        {
-            location: ['11', '12', '13'],
-            hit: ['', '', '']
-        },
-        {
-            location: ['69', '79'],
-            hit: ['', '']
-        },
-        {
-            location: ['32'],
-            hit: ['']
-        }
+
     ],
 
-    shipCount: 4,
+    shipCount: 0,
+
+    optionShip: {
+        count: [1, 2, 3, 4],
+        size: [4, 3, 2, 1]
+    },
+
+    collision: new Set(),
+
+    generateShip() {
+        for (let i = 0; i < this.optionShip.count.length; i++) {
+            for (let j = 0; j < this.optionShip.count[i]; j++) {
+                const size = this.optionShip.size[i];
+                const ship = this.generateOptionsShip(size);
+                this.ships.push(ship);
+                this.shipCount++;
+            }
+        }
+    },
+
+    generateOptionsShip(shipSize) {
+        const ship = {
+            hit: [],
+            location: [],
+        };
+
+        const direction = Math.random() < 0.5;
+        let x, y;
+
+        if (direction) {
+            x = Math.floor(Math.random() * 10);
+            y = Math.floor(Math.random() * (10 - shipSize));
+        } else {
+            x = Math.floor(Math.random() * (10 - shipSize));
+            y = Math.floor(Math.random() * 10);
+        }
+
+        for (let i = 0; i < shipSize; i++) {
+            if (direction) {
+                ship.location.push(x + '' + (y + i));
+            } else {
+                ship.location.push((x + i) + '' + y);
+            }
+
+            ship.hit.push('');
+        }
+
+        if (this.checkCollision(ship.location)) {
+            return this.generateOptionsShip(shipSize);
+        }
+
+        this.addCollision(ship.location);
+
+        return ship;
+    },
+
+    checkCollision(location) {
+        for (const coord of location) {
+            if (this.collision.has(coord)) {
+                return true;
+            }
+        }
+    },
+
+    addCollision(location) {
+        for (let i = 0; i < location.length; i++) {
+            const startCoordX = location[i][0] - 1;
+            //  console.log('startCoordX: ', startCoordX);
+
+            for (let j = startCoordX; j < startCoordX + 3; j++) {
+                const startCoordY = location[i][1] - 1;
+                // console.log('startCoordY: ', startCoordY);
+
+                for (let k = startCoordY; k < startCoordY + 3; k++) {
+
+                    if (j >= 0 && j < 10 && k >= 0 && k < 10) {
+                        const coord = j + '' + k;
+                        //  console.log('coord: ', coord);
+
+                        // Исправить в checkCollision в if includes на has
+                        this.collision.add(coord);
+
+                        /* Если не использовать в game.collision коллекцию new Set()
+                         if (!this.collision.includes(coord)) {
+                              this.collision.push(coord);
+                          } */
+                    }
+
+                }
+            }
+        }
+    }
 
 };
 
@@ -67,7 +143,7 @@ const fire = (event) => {
 
     const target = event.target;
 
-    if (game.shipCount == 0 || target.classList.length > 0 || target.tagName !== 'TD') return;
+    if (!game.shipCount || target.classList.length > 0 || target.tagName !== 'TD') return;
 
     show.miss(target);
     play.updateData = 'shot';
@@ -89,7 +165,7 @@ const fire = (event) => {
 
                 game.shipCount -= 1;
 
-                if (game.shipCount < 1) {
+                if (!game.shipCount) {
                     header.textContent = 'Игра окончена!';
                     header.style.color = 'red';
 
@@ -110,9 +186,18 @@ const fire = (event) => {
 const init = () => {
     enemy.addEventListener('click', fire);
     play.render();
+    game.generateShip();
     again.addEventListener('click', () => {
         location.reload();
     });
+
+    record.addEventListener('dblclick', () => {
+        localStorage.clear();
+        play.record = 0;
+        play.render();
+    });
+
+
 };
 
 init();
